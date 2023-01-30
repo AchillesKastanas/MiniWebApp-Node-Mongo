1a81d08ff7eac780791830028b561a8eec4fbce1
AchillesKastanas <achilleasrinos@gmail.com>
2023-01-30 13:48:42 +0200

    final fix

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use("/museums", museumsRouter);
 
 // Connect to MongoDB
 mongoose.connect(
-	"mongodb://127.0.0.1:27017/mydb",
+	process.env.MONGO_URI,
 	{ useNewUrlParser: true },
 	(err, connection) => {
 		// if (err) {
